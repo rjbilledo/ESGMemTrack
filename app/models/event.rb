f227ff289ed75ec24e5b663d77f039504ffcc5dd
Reed Billedo <rjbilledo@tamu.edu>
2022-11-16 12:30:06 -0600

    Made frontend changes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
     has_many :members, through: :attendances
     has_many :attendances
+
+    validates :event_name, :presence => true
+    validates :event_points, :presence => true
 end
